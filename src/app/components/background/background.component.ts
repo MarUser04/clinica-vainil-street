c4ac1a14b6a2331e3727a9fa83f425e66b02ea1b
Marco Useche <usechermarcoj@gmail.com>
2021-03-02 21:52:11 -0400

    new structure

---
+++ src/app/components/background/background.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.css']
+})
+export class BackgroundComponent implements OnInit {
+
+  @Input() backgroundColor: string;
+  @Input() imgURL: string;
+
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
