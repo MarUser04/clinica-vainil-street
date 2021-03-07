9c3ce753adf66a44b8f5f2edb5032f979f978f13
David Delgado <dadch13.14@gmail.com>
2021-03-07 00:14:58 -0400

    Migrate Background component styles to scss.

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -3,21 +3,19 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
-  styleUrls: ['./background.component.css']
+  styleUrls: ['./background.component.scss'],
 })
 export class BackgroundComponent implements OnInit {
-
   @Input() backgroundColor: string;
   @Input() imgURL: string;
   @Input() secondBg: string;
   private subMenuActive: boolean;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
   public subMenu() {
     this.subMenuActive = !this.subMenuActive;
   }
-
 }
