613b6511fc739e2d41a44aea7b3a54a07461ad7f
David Delgado <dadch13.14@gmail.com>
2021-03-07 00:16:18 -0400

    Migrate Specialities component styles to scss.

--- src/app/components/specialties/specialties.component.ts
+++ src/app/components/specialties/specialties.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-specialties',
   templateUrl: './specialties.component.html',
-  styleUrls: ['./specialties.component.css']
+  styleUrls: ['./specialties.component.scss'],
 })
 export class SpecialtiesComponent implements OnInit {
+  specialities = [
+    'Cardiología',
+    'Dermatología',
+    'Endocrinología',
+    'Gastroenterología',
+    'Ginecología',
+    'Odontología',
+    'Otorrinolaringología',
+    'Pediatría',
+    'Proctología',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
