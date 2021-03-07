44542f4c5650f9a93a8a6b496923876992933b54
David Delgado <dadch13.14@gmail.com>
2021-03-07 00:14:42 -0400

    Migrate About component styles to scss.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,21 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  private currentSelection = 'Junta Directiva';
+  private currentSelectionIcon = 'fas fa-user-cog';
 
-  private currentSelection: string = 'Junta Directiva';
-  private currentSelectionIcon: string = 'fas fa-user-cog';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public changeSelected(newSelected: string, newIcon: string): void {
     this.currentSelection = newSelected;
     this.currentSelectionIcon = newIcon;
   }
-
 }
