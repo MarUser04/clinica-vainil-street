75dc2bc93395bd37909f17c61199398457efe02a
David Delgado <dadch13.14@gmail.com>
2021-03-07 00:15:40 -0400

    Migrate JoinUs component styles to scss.

--- src/app/components/join-us/join-us.component.ts
+++ src/app/components/join-us/join-us.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-join-us',
   templateUrl: './join-us.component.html',
-  styleUrls: ['./join-us.component.css']
+  styleUrls: ['./join-us.component.scss'],
 })
 export class JoinUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
