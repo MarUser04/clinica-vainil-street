f7fb416cee1b6ac79a4200a1faaa85fac815fd10
David Delgado <dadch13.14@gmail.com>
2021-03-07 00:15:12 -0400

    Migrate DoctorView component styles to scss.

--- src/app/components/doctor-view/doctor-view.component.ts
+++ src/app/components/doctor-view/doctor-view.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-doctor-view',
   templateUrl: './doctor-view.component.html',
-  styleUrls: ['./doctor-view.component.css']
+  styleUrls: ['./doctor-view.component.scss'],
 })
 export class DoctorViewComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
